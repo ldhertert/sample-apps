768db9f7ef06b77f0a13d93eb4bf654d1a79c764
Luke Hertert <luke.hertert@gmail.com>
2022-10-27 14:00:29 -0400

    Update handler.js

--- serverless/src/handler.js
+++ serverless/src/handler.js
@@ -1,10 +1,11 @@
 "use strict";
-module.exports.hello = async (event) => {
+
+module.exports.func1 = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v2.0! Your function er executed successfully!",
+        message: "Luke's function",
         input: event,
       },
       null,
